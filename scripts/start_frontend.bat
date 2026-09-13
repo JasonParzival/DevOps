66f4429981537df66cd2077c1b124dd0b17b0cb9
JasonParzival <ruslanmuhamed123@gmail.com>
2026-09-13 14:38:45 +0800

    попытка запуска vue

--- scripts/start_frontend.bat
+++ scripts/start_frontend.bat
@@ -4,6 +4,6 @@ cd /d "%~dp0..\client"
 
 set JENKINS_NODE_COOKIE=dontKillMe
 
-start "Vue Server" /B npm.cmd run dev -- --host 0.0.0.0
+start "Vue Server" /B node node_modules\vite\bin\vite.js --host 0.0.0.0
 
 echo Vue server started.
