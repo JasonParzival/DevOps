0942e103151e447b47f1c9317dbff899eea5b574
JasonParzival <ruslanmuhamed123@gmail.com>
2026-09-13 14:23:28 +0800

    Исправление и попытка запуска

--- scripts/start_backend.bat
+++ scripts/start_backend.bat
@@ -2,6 +2,8 @@
 
 cd /d "%~dp0.."
 
+set JENKINS_NODE_COOKIE=dontKillMe
+
 start "Django Server" /B .venv\Scripts\python.exe manage.py runserver 0.0.0.0:8000
 
 echo Django server started.
--- scripts/start_frontend.bat
+++ scripts/start_frontend.bat
@@ -2,6 +2,8 @@
 
 cd /d "%~dp0..\client"
 
+set JENKINS_NODE_COOKIE=dontKillMe
+
 start "Vue Server" /B npm.cmd run dev -- --host 0.0.0.0
 
 echo Vue server started.
